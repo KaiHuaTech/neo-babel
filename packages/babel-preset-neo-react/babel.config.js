bd67b073368b306e5dd621ec07de9c21329479cf
soluteli <lancetcn@qq.com>
2019-06-15 14:36:42 +0800

    feat: 添加移除 proptypes 的插件

--- packages/babel-preset-neo-react/babel.config.js
+++ packages/babel-preset-neo-react/babel.config.js
@@ -1,3 +1,7 @@
-const config = require('./src/index');
+const getConfig = require('./src/index');
 
-module.exports = config
+module.exports = (api, options = {}) => {
+  api.cache(false)
+  const config = getConfig(api, options)
+  return config
+}
--- packages/babel-preset-neo-react/src/index.js
+++ packages/babel-preset-neo-react/src/index.js
@@ -1,6 +1,31 @@
-module.exports = {
-  presets: [
-    require('babel-preset-neo-core'),
-    '@babel/preset-react'
-  ]
+module.exports = (api, options = {}, _env) => {
+  const {
+    babelPresetNeoCoreConfig = {},
+    babelPresetReactConfig = {}
+  } = options
+
+  const env = _env || process.env.NODE_ENV || process.env.BABEL_ENV || 'development'
+
+  const isEnvProduction = env === 'production';
+
+
+  return {
+    presets: [
+      [require('babel-preset-neo-core'), babelPresetNeoCoreConfig],
+      ['@babel/preset-react', {
+        development: !isEnvProduction,
+        useBuiltIns: true,
+        ...babelPresetReactConfig
+      }]
+    ],
+    plugins: [
+      isEnvProduction && [
+        // Remove PropTypes from production build
+        require('babel-plugin-transform-react-remove-prop-types').default,
+        {
+          removeImport: true,
+        },
+      ],
+    ].filter(Boolean),
+  }
 }
